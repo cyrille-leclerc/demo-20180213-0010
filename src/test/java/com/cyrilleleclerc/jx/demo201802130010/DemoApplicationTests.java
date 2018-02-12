c046e46be7fbb6f751f405ab4a13014e7076f8ef
Cyrille Le Clerc <cleclerc@cloudbees.com>
2018-02-13 00:10:56 +0100

    Initial import

---
+++ src/test/java/com/cyrilleleclerc/jx/demo201802130010/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cyrilleleclerc.jx.demo201802130010;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
